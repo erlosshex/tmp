c84e5fdd2ee34e0bcda11714dcda023655b191f8
erlosshex <gugsabr@gmail.com>
2016-11-09 20:53:11 +0800

    cin.get()

---
+++ myfirst.cpp
@@ -0,0 +1,18 @@
+//myfirst.cpp -- displays a message
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  using namespace std;
+  cout<<"Come up and C++ me some time.";
+  cout<<endl;
+
+  cout<<"You won't regret it!"<<endl;
+
+  char c;  
+  c=cin.get();
+  cout<<"c --> "<<c<<endl;
+  return 0;
+}
