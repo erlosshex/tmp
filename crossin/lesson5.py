f57af757261f572ef600b82f35b7ee77effd1578
erlosshex <gugsabel@163.com>
2017-02-17 20:55:04 +0800

    crossin

---
+++ crossin/lesson5.py
@@ -0,0 +1,35 @@
+num=123.456
+print num
+
+num='hello'
+print num
+
+print 'Who do you think I am?'
+you=input()
+print you
+
+
+name='Crossin'
+myVar=123
+price=5.99
+visible=True
+
+print name
+
+a=123
+print a
+a='hi'
+print a
+
+value=3*4
+print value
+value=2<5
+print value
+
+name=input()
+print name
+
+print 'Who do you think I am?'
+you=input()
+print 'Oh, yes! I am a'
+print you
